137845cafd463eee838fc08bb3a3b42656854832
krishkansal7 <krish1412.be20@chitkara.edu.in>
2022-08-11 23:18:34 +0530

    commit6

--- script.js
+++ script.js
@@ -45,3 +45,25 @@ menu.onclick = () =>{
   navbar.classList.toggle('active');
   
 }
+
+
+
+let btn=(document.querySelector('.btn1'))
+btn.addEventListener('click',(e)=>{
+    e.preventDefault()     
+const textvalue1=document.querySelector('.inpt1').value
+console.log(textvalue1);
+
+const textvalue2=document.querySelector('.inpt2').value
+console.log(textvalue2);
+
+const textvalue3=document.querySelector('.inpt3').value
+console.log(textvalue3);
+
+const textvalue4=document.querySelector('.inpt4').value
+console.log(textvalue4);
+
+const textvalue5=document.querySelector('.inpt5').value
+console.log(textvalue5);
+})
+
